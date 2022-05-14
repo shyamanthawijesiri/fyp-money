853f0977d3b51d5f4d09ed14ef1c750b88b8487d
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2022-05-14 19:22:25 +0530

    show prediction

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 import cv2
 import os
+import predict_model as model
 
 def cvtRGB(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -10,7 +11,6 @@ def resized(img):
     return cv2.resize(img,(size, size))
 heading = f'<h1 style="font-weight:bold;text-align:center;">Topic</h1>'
 st.markdown(heading, unsafe_allow_html=True)
-# st.title('Betel vine Leaves Categorization and Disease Detection')
 fs = st.file_uploader('upload Image',['jpg','png','jpeg'])
 save_path = 'images/upload/'
 
@@ -28,11 +28,7 @@ if fs is not None:
     img = cv2.imread(img_path)
     resized_img = resized(img)
     display_img = cvtRGB(resized_img)
-    # display_img = re
-    # image = Image.open(img_path)
-    # image2 = image.rotate(-90)
 
-    # st.image(image2)
     if fs:
         col1, col2, col3 = st.columns([1, 6, 1])
         with col1:
@@ -47,12 +43,12 @@ if fs is not None:
         input_img = os.path.join(save_path,fs.name)
 
 
-        # result = healthy_classification.healthyLeafClassification(input_img)
-        # if result == 0:
-        #     new_title = '<p style="color:Green; font-size: 32px;font-weight:bold; text-align: center;">valid money</p>'
-        #     st.markdown(new_title, unsafe_allow_html=True)
-        #
-        # else:
-        #     new_title = '<p style="color:red; font-size: 32px; font-weight:bold; text-align: center;">Unhealthy Leaf</p>'
-        #     st.markdown(new_title, unsafe_allow_html=True)
-        #     print("unhealthy leaf")
+        result = model.moneyClassification(input_img)
+        if result == 0:
+            new_title = '<p style="color:Green; font-size: 32px;font-weight:bold; text-align: center;">Valid Money</p>'
+            st.markdown(new_title, unsafe_allow_html=True)
+
+        else:
+            new_title = '<p style="color:red; font-size: 32px; font-weight:bold; text-align: center;">Invalid Money</p>'
+            st.markdown(new_title, unsafe_allow_html=True)
+
